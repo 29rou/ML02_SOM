64431c2fa55e0a6349f58ce4b8c9113f3bee3a49
Yoshiki Sakamoto <c171401r@icu.ac.jp>
2016-01-29 21:21:48 +0900

    さらにリファクタリングした

--- som.hs
+++ som.hs
@@ -26,44 +26,49 @@ vUpdate img pixel index = let index' = index*3
                               mod' m = ((mod index 50)/= m)
                               (vExtract',empty') = (vExtract img pixel,(empty,empty))
                               if' cod ind x  = if cod then vExtract' ind x else empty'
-                              (iup,up)= if' (index>=50) (index-50) 2
-                              (iup',up')= if' (index>=100) (index-100) 10
-                              (idown,down)= if' (index<=2449) (index+50) 2
-                              (idown',down')= if' (index<=2399) (index+100) 10
-                              (iright,right)= if' (mod' 49) (index+1) 2
-                              (iright',right')= if' ((mod' 49)&&(mod' 48)) (index+2) 10
-                              (ileft,left)= if' (mod' 0) (index-1) 2
-                              (ileft',left')= if' ((mod' 0)&&(mod' 1)) (index-2) 10
-                              (iupright,upright)= if' (index>=50 && mod' 49) (index-49) 5
-                              (iupleft,upleft)= if' (index>=50 && mod' 0) (index-51) 5
-                              (idownright,downright)= if' (index<=2449 && mod' 49) (index+51) 5
-                              (idownleft,downleft)= if' (index<=2449 && mod' 0) (index+49) 5
+                              (iup,up)= if' (index>=50) (index-50) 5
+                              (iup',up')= if' (index>=100) (index-100) 11
+                              (idown,down)= if' (index<=2449) (index+50) 5
+                              (idown',down')= if' (index<=2399) (index+100) 11
+                              (iright,right)= if' (mod' 49) (index+1) 5
+                              (iright',right')= if' ((mod' 49)&&(mod' 48)) (index+2) 11
+                              (ileft,left)= if' (mod' 0) (index-1) 5
+                              (ileft',left')= if' ((mod' 0)&&(mod' 1)) (index-2) 11
+                              (iupright,upright)= if' (index>=50 && mod' 49) (index-49) 10
+                              (iupleft,upleft)= if' (index>=50 && mod' 0) (index-51) 10
+                              (idownright,downright)= if' (index<=2449 && mod' 49) (index+51) 10
+                              (idownleft,downleft)= if' (index<=2449 && mod' 0) (index+49) 10
                               change = pixel' V.++ up V.++ up' V.++ down V.++ down' V.++ right V.++  right' V.++ left V.++ left' V.++ upright V.++ upleft V.++ downright V.++ downleft
                               target = lis V.++ iup V.++ iup' V.++ idown V.++ idown' V.++ iright V.++  iright' V.++ ileft V.++ ileft' V.++ iupright V.++ iupleft V.++ idownright V.++ idownleft
                           in update_ img target (change)
 
-mainloop:: ViewPort -> Float -> Vector Int -> IO (Vector Int)
-mainloop _ _ img = do let leng = V.length img 
-                      if leng == (7500)
-                        then do putStr "STOP" ; hFlush stdout; tex <- getLine; print tex;return ()
-                        else do print $(leng - 7500); return ()
-                      let (img', file) = V.splitAt (7500) img
-                      findex <- (randomRIO (0,(div ((V.length file)-3) 3)) :: IO Int)
-                      let findex' = (findex*3)
-                          pixel2 = L.map (file!)[(findex*3), ((findex*3)+1), ((findex*3)+2)]
-                          pixel = L.cycle pixel2
-                          dist = sum' $ L.zipWith (-) (toList img') pixel
-                          indexL = L.elemIndices (L.minimum dist) dist
-                      index <-do i <- randomRIO(0,((L.length indexL)-1)):: IO Int
-                                 return (indexL !! i)
-                      let result = vUpdate img' (fromList pixel2) index
-                          filed = (V.take (findex') file) V.++ (V.drop (findex'+3) file)
-                      hFlush stdout
-                      return $ result V.++ filed
-                      where
+mainloop':: Vector Int -> IO (Vector Int)
+mainloop' img = do let (img', file) = V.splitAt (7500) img
+                   findex <- (randomRIO (0,(div ((V.length file)-3) 3)) :: IO Int)
+                   let findex' = (findex*3)
+                       pixel2 = L.map (file!)[(findex*3), ((findex*3)+1), ((findex*3)+2)]
+                       pixel = L.cycle pixel2
+                       dist = sum' $ L.zipWith (-) (toList img') pixel
+                       indexL = L.elemIndices (L.minimum dist) dist
+                   index <-do i <- randomRIO(0,((L.length indexL)-1)):: IO Int
+                              return (indexL !! i)
+                   let result = vUpdate img' (fromList pixel2) index
+                       filed = (V.take (findex') file) V.++ (V.drop (findex'+3) file)
+                   return $ result V.++ filed
+                   where
                         sum' :: [Int] -> [Int]
                         sum' (x1:x2:x3:xs) = (abs x1)+(abs x2)+(abs x3) :sum' xs
                         sum' _ = []
+
+mainloop:: ViewPort -> Float -> Vector Int -> IO (Vector Int)
+mainloop _ _ img = do let leng = V.length img 
+                          mainloop = 
+                      img' <- if leng == (7500)
+                                  then do putStr "STOP" ; return (img)
+                                  else do print $(leng - 7500); im <- mainloop' img ;return (im)
+                      hFlush stdout
+                      return img'
+                      
 
 main :: IO()
 main = do 
@@ -71,7 +76,7 @@ main = do
     args <- getArgs
     file <- loadImage (L.head args)
     let somdata = input V.++ file
-    simulateIO (InWindow "test" (800, 800) (0, 0)) white 100 somdata showImg mainloop
+    simulateIO (InWindow "test" (800, 800) (0, 0)) white 500 somdata showImg mainloop
     where 
         intialize :: IO (Vector Int)
         intialize = let rseed = getStdRandom $ randomR (0,255) :: IO Int
